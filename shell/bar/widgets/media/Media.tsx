0bce9bb2b026f32711dabcb7f16507cf0015d48d
Ozoku <19756856+ozokuz@users.noreply.github.com>
2025-12-01 03:43:51 +0200

    updated media control section, still a bit wonky

--- shell/bar/widgets/media/Media.tsx
+++ shell/bar/widgets/media/Media.tsx
@@ -1,4 +1,4 @@
-import { createBinding, createComputed, With } from "ags";
+import { Accessor, createState, createBinding, createComputed, With, For } from "ags";
 import app from "ags/gtk4/app";
 import Mpris from "gi://AstalMpris";
 import style from "./Media.scss";
@@ -7,34 +7,42 @@ const mpris = Mpris.get_default();
 
 app.apply_css(style);
 
-function MediaActual({ player }: { player: Mpris.Player }) {
-  const songLabel = createComputed(
-    [createBinding(player, "artist"), createBinding(player, "title")],
-    (artist, title) =>
-      !artist && !title ? "Nothing is playing" : `${title} - ${artist}`
-  );
+type MediaVisualProps = {
+  songLabel: Accessor<string>;
+  coverArt: Accessor<string>;
+  playbackStatus: Accessor<boolean>;
+  previous: () => void;
+  next: () => void;
+  playPause: () => void;
+};
 
+function MediaVisual({
+  songLabel,
+  coverArt,
+  next,
+  playPause,
+  previous,
+  playbackStatus,
+}: MediaVisualProps) {
   return (
     <box>
       <box spacing={8} class="media-buttons">
-        <button class="base" onClicked={() => player.previous()}>
+        <button class="base" onClicked={previous}>
           <label label="󰒮" class="media-icon" />
         </button>
-        <button class="base play" onClicked={() => player.play_pause()}>
+        <button class="base play" onClicked={playPause}>
           <label
-            label={createBinding(player, "playbackStatus").as((s) =>
-              s === Mpris.PlaybackStatus.PLAYING ? "󰏦" : "󰐍"
-            )}
+            label={playbackStatus.as((s) => (s ? "󰏦" : "󰐍"))}
             class="media-icon"
           />
         </button>
-        <button class="base" onClicked={() => player.next()}>
+        <button class="base" onClicked={next}>
           <label label="󰒭" class="media-icon" />
         </button>
       </box>
       <box
         class="cover-art"
-        css={createBinding(player, "coverArt").as((cover) => {
+        css={coverArt.as((cover) => {
           if (!cover) {
             return `background-image: linear-gradient(0deg, #777, #777), linear-gradient(0deg, #333, #333);`;
           }
@@ -47,14 +55,44 @@ function MediaActual({ player }: { player: Mpris.Player }) {
   );
 }
 
+function MediaFake() {
+  const [coverArt] = createState(false);
+  const [playbackStatus] = createState(false);
+
+  return <MediaVisual songLabel="Nothing is playing" coverArt={coverArt} next={() => {}} playPause={() => {}} previous={() => {}} playbackStatus={playbackStatus} />
+}
+
+function MediaActual({ player }: { player: Mpris.Player }) {
+  const songLabel = createComputed(
+    [createBinding(player, "artist"), createBinding(player, "title")],
+    (artist, title) =>
+      !artist && !title ? "Nothing is playing" : `${title} - ${artist}`
+  );
+  const coverArt = createBinding(player, "coverArt");
+  const playbackStatus = createBinding(player, "playbackStatus").as((s) =>
+    s === Mpris.PlaybackStatus.PLAYING
+  );
+
+  return <MediaVisual songLabel={songLabel} coverArt={coverArt} next={() => player.next()} playPause={() => player.play_pause()} previous={() => player.previous()} playbackStatus={playbackStatus} />
+}
+
 export default function Media() {
   const player = createBinding(mpris, "players").as(
-    (players) => players.find((p) => p.busName.endsWith("playerctld"))!
+    (players) => players.filter((p) => !p.busName.endsWith("playerctld"))
+  );
+  const hasPlayers = player.as(
+    (players) => players.length !== 0
   );
 
   return (
     <box class="media">
-      <With value={player}>{(player) => <MediaActual player={player} />}</With>
+      <For each={player}>
+        {(player) =>
+          <MediaActual player={player} />          
+        }
+      </For>
+      <With value={hasPlayers}>{(has) => has ? null : <MediaFake />}</With>
     </box>
   );
 }
+
